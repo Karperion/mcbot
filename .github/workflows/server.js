d442f41cbc8cda3e8686682ba5a0835d3da1e070
Karperion <75799561+Karperion@users.noreply.github.com>
2021-01-06 01:27:02 +0300

    Update server.js

--- .github/workflows/server.js
+++ .github/workflows/server.js
@@ -4,7 +4,7 @@ var db = require("quick.db");
 var ayar = {
   host: "karperion.aternos.me", //Sunucu IPnizi "" arasına giriniz.
   port: 25565, //Sunucu portunuzu giriniz. Genellikle 25565 olarak ayarlıdır.
-  username: "HosGeldiniz", //Sunucuya giriş yapacak bot isminizi "" arasına girin.
+  username: "HosGeldinizz", //Sunucuya giriş yapacak bot isminizi "" arasına girin.
   version: false //Burası böyle kalsın değiştirmeyin.
 };
   
